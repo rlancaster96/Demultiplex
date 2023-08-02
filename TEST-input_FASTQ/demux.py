eb2578517350e1b4d7d25e79a51056df48d9c9f3
rlancaster96 <rubenl@uoregon.edu>
2023-08-02 15:01:01 -0700

    halfway update demux.py

--- TEST-input_FASTQ/demux.py
+++ TEST-input_FASTQ/demux.py
@@ -14,7 +14,7 @@ def get_args():
     parser.add_argument("-R2", "--index1", help="Specify the file name for index 1", required=True, type=str)
     parser.add_argument("-R3", "--index2", help="Specify the file name for index 2", required=True, type=str)
     parser.add_argument("-R4", "--read2", help="Specify the file name for read 2", required=True, type=str)
-    # parser.add_argument("-o", "--writename", help="Specify output file name", required=False, type=str)
+    parser.add_argument("-c", "--cutoff", help="Specify quality cutoff for indexes", required=False, type=int)
     return parser.parse_args()
 
 args = get_args()
@@ -32,12 +32,38 @@ args = get_args()
 index_names: dict = {"A1":"ATCG", "B2":"GGGG"}
 rcindex: dict = bioinfo.makedict(index_names)
 
-# open files 
+# open read files 
 read1 = open(args.read1, "r")
 read2 = open(args.read2, "r")
 index1 = open(args.index1, "r")
 index2 = open(args.index2, "r")
 
+# open write files
+R1_hopped = open("R1_hopped.fastq", "w")
+R4_hopped = open("R4_hopped.fastq", "w")
+R1_unknown = open("R1_unknown.fastq", "w")
+R4_unknown = open("R4_unknown.fastq", "w")
+
+handles: list = [R1_hopped, R4_hopped, R1_unknown, R4_unknown] #make a list of all filehandles to close at the end 
+indexhandledict: dict = {}
+#make filehandles and dictionary of filehandles for paired indexes where key = index, value = filehandle
+for index in index_names:
+    filehandle1 = "fh_R1_" + index
+    filename1 = "R1_index_" + index + ".fastq"
+    
+    filehandle2 = "fh_R4_" + index
+    filename2 = "R4_index_" + index + ".fastq"
+    
+    filehandle1 = open(filename1, "w")
+    filehandle2 = open(filename2, "w")
+
+    handles.append(filehandle1) #append filehandle names to the handles file for final closing
+    handles.append(filehandle2)
+
+    pack: tuple = (filehandle1, filehandle2) #add key = index, filehandles1 and 2 = values for each index. use this for writing paired indexes later.
+    indexhandledict[index] = pack
+
+#start looping through files
 while True: 
 
 #store 4 records at a time (one from each file) to temporary memory 
@@ -69,7 +95,7 @@ while True:
     #strip newline characters and make index variables
     ind1 = index1_seq.strip()
     ind2 = index2_seq.strip()
-    rcind2 = bioinfo.rev_comp(ind2)
+    rcind2 = bioinfo.rev_comp(ind2) #make reverse complement of index 2
     appender = " " + ind1 + "-" + rcind2
     read1_header = read1_header + appender
     read2_header = read2_header + appender
@@ -77,25 +103,47 @@ while True:
     index1_qscore = index1_qscore.strip()
     index2_qscore = index2_qscore.strip()
 
-
+    #1 : QSCORE FILTERING 
+    #*************************************
     #get lowest qscore from indexes 
     lowscore1 = 100
     lowscore2 = 100
-    for a in index1_qscore:
-        qscore1 = bioinfo.convert_phred(index1_qscore)
-        if qscore1 < lowscore1:
-            qscore1 = lowscore1
-    for a in index2_qscore:
-        qscore2 = bioinfo.convert_phred(index2_qscore)
-        if qscore2 < lowscore2:
-            qscore2 = lowscore2
     
-    print(lowscore1, lowscore2)
+    for a in index1_qscore:
+        qscore1 = bioinfo.convert_phred(a)
+        if qscore1 < lowscore1:
+            lowscore1 = qscore1
+    for a in index2_qscore:
+        qscore2 = bioinfo.convert_phred(a)
+        if qscore2 < lowscore2:
+            lowscore2 = qscore2
+    # write to unknown file if it doesn't make the cutoff
+    if lowscore1 < args.cutoff or lowscore2 < args.cutoff: 
+        R1_unknown.write(f"{read1_header}\n{read1_seq}{read1_comment}{read1_qscore}")
+        R4_unknown.write(f"{read2_header}\n{read2_seq}{read2_comment}{read2_qscore}")
+    
+    #2 : DICTIONARY COMPARISON
+    #*************************************
+    #write to unknown file if index1 or rev comp of index 2 is not a key in the rcindex dictionary:
+    elif ind1 not in rcindex or rcind2 not in rcindex:
+        R1_unknown.write(f"{read1_header}\n{read1_seq}{read1_comment}{read1_qscore}")
+        R4_unknown.write(f"{read2_header}\n{read2_seq}{read2_comment}{read2_qscore}")
+    
+    #3 : PAIRED INDEXES
+    #*************************************
+    #write to paired indexes file if indexes match 
+    # elif ind1 == rcind2:
+
+
+
+
+
+        
+
+
 
 
 
 #close files
-read1.close()
-read2.close()
-index1.close()
-index2.close()
+for handle in handles:
+    handle.close()
